bddbb29fc4861f113e807e350df2851f7da94a4a
Iman <imanmozafari.neonser@gmail.com>
2024-09-03 03:15:28 -0700

    create url serializer

--- shortner/serializers.py
+++ shortner/serializers.py
@@ -1,1 +1,8 @@
-from rest_framework import urlpatterns
+from rest_framework import serializers
+from .models import Url
+
+class Url_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Url
+        fields = '__all__'
+        read_only_fields = ['short_url', 'clicks']
