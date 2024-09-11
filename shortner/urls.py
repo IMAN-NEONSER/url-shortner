4a29770a15c5b47fa01fcffe6ea48365c772277c
Iman <imanmozafari.neonser@gmail.com>
2024-09-12 00:03:51 +0330

    create UrlRedirectView view and url for it

--- shortner/urls.py
+++ shortner/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'shortner'
 urlpatterns = [
     path('shortner/', views.UrlShortnerView.as_view(), name='url_shortner'),
+    path('<str:short_url>/', views.UrlRedirectView.as_view(), name='url-redirect'),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Url
 from rest_framework import status
 from .serializers import UrlSerializer
 
@@ -12,4 +13,12 @@ class UrlShortnerView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UrlRedirectView(APIView):
+    def get(self, request, short_url):
+        url = get_object_or_404(Url, short_url=short_url)
+        url.clicks += 1
+        url.save()
+        return redirect(url.original_url)
+
 
